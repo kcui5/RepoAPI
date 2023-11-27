c4c02cec7c0c1ade061c02583c14fcd2b0778ed1
Kyle <kyle.tz.cui@gmail.com>
2023-11-26 17:54:58 -0800

    idk

--- flask_deploy.py
+++ flask_deploy.py
@@ -1,5 +1,6 @@
 import flask
 from typing import Dict
+import os
 
 import main
 
@@ -7,6 +8,21 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def repoapi_entry():
+    auth_header = flask.request.headers.get('Authorization')
+
+    if not auth_header:
+        flask.abort(401, description="Authorization header is missing")
+
+    parts = auth_header.split()
+
+    if parts[0].lower() != 'bearer' or len(parts) != 2:
+        flask.abort(401, description="Authorization header must be Bearer token")
+
+    api_key = parts[1]
+    if api_key != os.environ.get('REPOAPI_MODAL_KEY'):
+        print("Invalid API key request")
+        flask.abort(403, description="Invalid API key")
+
     input: Dict = flask.request.json
     print(input)
     repo_link = input['repo_link']
